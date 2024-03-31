d58b76c076a5490828fe50619e1f774569b6539e
jonaturn <65528343+jonaturn@users.noreply.github.com>
2024-03-31 16:26:01 +0000

    first iteration

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Jonathen Cheng Yuzhe",
+  title: "Computer Science Undergraduate",
+  email: "jonathencheng86@gmail.com",
+  gitHub: "https://github.com/jonaturn/portfolio",
+  linkedIn: "https://www.linkedin.com/in/jonathen-cheng-03b3a2158/edit/forms/intro/new/?profileFormEntryPoint=PROFILE_SECTION",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/photo1711899355.jpeg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,29 +31,34 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Fullstack Intern at Lumens Group",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "My journey as a fullstack intern at Lumens.",
+    url: "",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Software Developer at Ngee Ann Polytechnic",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "How I spent my holidays as a software developer at Ngee Ann Polytechnic.",
+    url: "",
   },
   {
-    title: "My Resume Site",
+    title: "NUS HacknRoll 2024",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A short story about my first hackathon.",
+    url: "",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "NUS IEEE AWS DeepRacer 2024",
+    description: 
+      "How my friends and I achieved third place at our second hackathon.",
+    url: "",
   },
+  {
+    title: "Bartending",
+    description: "Pursuing the art of mixology at a bespoke, speakeasy bar.",
+    url: "",
+  }
 ];
 
 const Portfolio = () => {
